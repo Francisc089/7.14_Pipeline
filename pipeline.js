5e0285c8fbc4c54ab3767de149b92d88c924d2d1
Francisco Munoz <fmunozpena@gmail.com>
2018-07-14 17:55:22 -0400

    progress so far

--- pipeline.js
+++ pipeline.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const operations = {
     set: (input, arg )=> {
       return arg*1;
@@ -20,13 +23,71 @@ const operations = {
 
 const pipelineSync = function(str) {
   var strArray = str.split('|');
-  console.log(strArray) 
+
+  var count = 0;
+
   strArray.forEach(function(element) {
-    var wordArray = element.split(' ')
+    let wordArr = element.trim().split(' ')
+    console.log(wordArr)
+    count = operations[wordArr[0]](count, wordArr[1])
   })   
 
-  return str;
+  return count;
 }
-console.log('hello')
 
-module.exports = pipelineSync;
+const operations2 = {
+  addFromFile: (input, arg, fn)=> {
+    if(typeof input === 'undefined'){
+      return fn('input is required');
+    }
+    fs.readFile(arg, (err, result)=> {
+      //console.log('fs ',fs.readFile())
+      fn(err, input + result.toString()*1);
+    });
+  },
+  set: (input, arg, fn) => {
+    fn(null, arg*1);
+  },
+  add: (input, arg, fn)=> {
+    if(typeof input === 'undefined'){
+      return fn('input is required');
+    }
+    fn(null, input + arg*1);
+  },
+  mult: (input, arg, fn)=> {
+    if(typeof input === 'undefined'){
+      return fn('input is required');
+    }
+    fn(null, input * arg*1);
+  }
+};
+
+const callback = function(arg, operation) {
+  return operation;
+}
+
+const pipelineAsync = function(str) {
+  
+  var strArray = str.split('|');
+
+  var count = 0;
+
+  strArray.forEach(function(element) {
+    let wordArr = element.trim().split(' ')
+    console.log(wordArr)
+    if(wordArr[0] === 'addFromFile') {
+      const fileName = path.join(__dirname, wordArr[1]);
+      console.log('fileName', fileName)
+      count = operations2[wordArr[0]](count, fileName, callback)
+      console.log('count file' , count);
+      
+    }
+    else {
+      count = operations2[wordArr[0]](count, wordArr[1], callback)
+      
+    }
+  })   
+
+  return count;
+}
+module.exports = {pipelineSync, pipelineAsync}
--- test/pipeline_spec.js
+++ test/pipeline_spec.js
@@ -1,13 +1,29 @@
 const expect = require('chai').expect;
-const pipelineSync = require('../pipeline');
+const obj = require('../pipeline');
+const pipelineSync = obj.pipelineSync;
+const pipelineAsync = obj.pipelineAsync;
+const file = require('../foo.txt');
+const path = require('path');
 
-describe('pipeline', () => {
+describe('Sync Pipeline', () => {
     it('function exists', () => {
         expect(pipelineSync).to.be.ok;
     });
-    it('splits a string', () => {
+    it('splits a string and adds', () => {
         //expect(pipelineSync).to.be.ok;
         expect(pipelineSync('set 1 | add 1')).to.equal(2);
     });
+    it('does it all', () => {
+        expect(pipelineSync('set 1 | add 1 | mult 21')).to.equal(42);
+    })
 });
 
+describe('Async Pipeline', () => {
+    it('function exists', () => {
+        expect(pipelineAsync).to.be.ok;
+    });
+    it('adds from file', () => {
+        const fileName = path.join(__dirname, "../foo.txt")
+        expect(pipelineAsync('set 1 | addFromFile ../foo.txt')).to.equal(2)
+    })
+});
